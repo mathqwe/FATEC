d77b3073b2fef69528806f8735b481b9467db07f
mathqwe <mathqwe.santana@gmail.com>
2025-05-13 05:27:35 -0300

    Adicionado exercícios de alocação dinâmica

--- algoritmos/C/gerenciamento-memoria/lista/concatenar-strings/main.c
+++ algoritmos/C/gerenciamento-memoria/lista/concatenar-strings/main.c
@@ -8,17 +8,19 @@
 
 char *concatena(char *s1, char *s2){
   char *sres = NULL;
-  int t = strlen(s1) + strlen(s2);
-  int i = 0;
-
+  int t = strlen(s1) + strlen(s2) - 2;//Tamanho total de índices do sres
   sres = malloc(t * sizeof(char));
-  
-  do{
-    
+  int j = strlen(s1) - 2;//Indices da primeira string sem o \0
+
+  for(int i = 0; i < (strlen(s1) - 2); i++){
+    sres[i] = s1[i];
   }
 
-  for(int j = 0; (j < MAX && s2[j] != '\n'); j++){
-    sres[i] = s2[j];
+  int k = 0;
+
+  for(j; j <= t; j++){
+    sres[j] = s2[k];
+    k++;
   }
 
   return sres;
@@ -27,6 +29,7 @@ char *concatena(char *s1, char *s2){
 int main(void){
   char s1[MAX];
   char s2[MAX];
+  char *s3;
 
   printf("Esse programa lê duas strings e concatena seus valores em outra string usando alocação dinâmica da memória\n");
   printf("\nEntre com a primeira string s1: \n");
@@ -35,8 +38,9 @@ int main(void){
   printf("\nEntre com a segunda string s2: \n");
   fgets(s2, MAX, stdin);
  
-
-  printf(concatena(s1,s2),"\n\n");
+  s3 = concaten(s1, s2)
+  printf("%s\n\n", s3);
+  free(s3);
 
   return 0;
 }
