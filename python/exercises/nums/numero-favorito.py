3dd9b8a9d396487391424f2e5b67023fdd8ec221
mathqwe <mathqwe.santana@gmail.com>
2025-08-27 20:23:53 +0000

    exercicios de vetores em C

--- python/exercises/nums/numero-favorito.py
+++
@@ -1,8 +0,0 @@
-"""
-Armazene seu número favorito em uma variável. Em
-seguida, usando essa variável, crie uma mensagem que revele o seu número
-favorito. Exiba essa mensagem.
-"""
-
-num = 14
-print("Meu número favorito é: " + str(num))
